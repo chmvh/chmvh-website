64876665551b865f73a1acae392cf442853ff295
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-01 23:16:38 -0400

    Add services page.

--- chmvh_website/staticpages/urls.py
+++ chmvh_website/staticpages/urls.py
@@ -3,10 +3,12 @@ from django.views.generic import TemplateView
 
 
 homepage_view = TemplateView.as_view(template_name='staticpages/index.html')
+services_view = TemplateView.as_view(template_name='staticpages/services.html')
 team_view = TemplateView.as_view(template_name='staticpages/team.html')
 
 
 urlpatterns = [
     url(r'^$', homepage_view, name='homepage'),
+    url(r'^services/$', services_view, name='services'),
     url(r'^team/$', team_view, name='team'),
 ]
