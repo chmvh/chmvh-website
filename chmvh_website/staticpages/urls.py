8fc36bcee4ac69907e5f268716dfa90abcc46850
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-02 16:02:02 -0400

    Add housecalls page.

--- chmvh_website/staticpages/urls.py
+++ chmvh_website/staticpages/urls.py
@@ -3,12 +3,15 @@ from django.views.generic import TemplateView
 
 
 homepage_view = TemplateView.as_view(template_name='staticpages/index.html')
+housecalls_view = TemplateView.as_view(
+    template_name='staticpages/housecalls.html')
 services_view = TemplateView.as_view(template_name='staticpages/services.html')
 team_view = TemplateView.as_view(template_name='staticpages/team.html')
 
 
 urlpatterns = [
     url(r'^$', homepage_view, name='homepage'),
+    url(r'^housecalls/$', housecalls_view, name='housecalls'),
     url(r'^services/$', services_view, name='services'),
     url(r'^team/$', team_view, name='team'),
 ]
