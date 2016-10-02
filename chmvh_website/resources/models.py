7e8a63c2049c3087939c1e4480c1e12fba8c2aa6
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-02 18:52:59 -0400

    Add resource model.

--- chmvh_website/resources/models.py
+++ chmvh_website/resources/models.py
@@ -7,3 +7,31 @@ class Category(models.Model):
         max_length=100,
         unique=True,
         verbose_name='resource category')
+
+
+class Resource(models.Model):
+    """A resource containing various information."""
+    address = models.TextField(
+        blank=True,
+        verbose_name='address')
+    description = models.TextField(
+        blank=True,
+        verbose_name='description')
+    email = models.EmailField(
+        blank=True,
+        verbose_name='email address')
+    phone = models.CharField(
+        blank=True,
+        max_length=50,
+        verbose_name='phone number')
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name='title')
+    url = models.URLField(
+        blank=True,
+        verbose_name='website URL')
+
+    def __str__(self):
+        """Return the resource's title"""
+        return self.title
--- chmvh_website/resources/tests/test_models.py
+++ chmvh_website/resources/tests/test_models.py
@@ -13,3 +13,38 @@ class TestCategoryModel(TestCase):
         category = models.Category.objects.create(title='New Category')
 
         self.assertEqual('New Category', category.title)
+
+
+class TestResourceModel(TestCase):
+
+    def test_creation(self):
+        """Test creating a new resource.
+
+        A resource should require a title, but be able to accept a
+        variety of different information.
+        """
+        resource = models.Resource.objects.create(
+            title='Resource',
+            description='Test resource.',
+            phone='(555) 555-5555',
+            address='123 Example Drive\nNew York, NY 12345',
+            url='http://example.com',
+            email='test@example.com')
+
+        self.assertEqual('Resource', resource.title)
+        self.assertEqual('Test resource.', resource.description)
+        self.assertEqual('(555) 555-5555', resource.phone)
+        self.assertEqual(
+            '123 Example Drive\nNew York, NY 12345', resource.address)
+        self.assertEqual('http://example.com', resource.url)
+        self.assertEqual('test@example.com', resource.email)
+
+    def test_string_conversion(self):
+        """Test converting a resource to a string.
+
+        Converting a resource to a string should return the resource's
+        title.
+        """
+        resource = models.Resource(title='Test Resource')
+
+        self.assertEqual(resource.title, str(resource))
