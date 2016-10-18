b6eaf61bf616338aba922ac2164a1e49ffb35548
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-18 14:03:19 -0400

    Add in memoriam view.

--- chmvh_website/gallery/urls.py
+++ chmvh_website/gallery/urls.py
@@ -5,6 +5,7 @@ from gallery import views
 
 urlpatterns = [
     url(r'^$', views.GalleryIndexView.as_view(), name='index'),
+    url(r'^memoriam/$', views.PetMemoriamView.as_view(), name='pet-memoriam'),
     url(r'^(?P<first_letter>[a-zA-Z])/$', views.PetListView.as_view(),
         name='pet-list'),
 ]
--- chmvh_website/gallery/views.py
+++ chmvh_website/gallery/views.py
@@ -39,11 +39,28 @@ class PetListView(generic.base.TemplateView):
         context = super(PetListView, self).get_context_data(*args, **kwargs)
 
         letter = self.kwargs.get('first_letter').upper()
-        context['letter'] = letter
+        context['category'] = letter
 
         pets = models.Patient.objects.filter(
             first_letter__iexact=letter).exclude(
             deceased=True)
+        context['pets'] = pets
+
+        context.update(patient_context())
+
+        return context
+
+
+class PetMemoriamView(generic.base.TemplateView):
+    template_name = 'gallery/pet-list.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(PetMemoriamView, self).get_context_data(
+            *args, **kwargs)
+
+        context['category'] = 'In Memoriam'
+
+        pets = models.Patient.objects.filter(deceased=True)
         context['pets'] = pets
 
         context.update(patient_context())
