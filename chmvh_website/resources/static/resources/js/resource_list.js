c48bc39f1d9f32ee6e4fbbd4e37cd2acb849a1ef
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-19 00:17:50 -0400

    Refactor resource list to be reusable.
    The css and js behind the resource list has been refactored into a
    reusable component called an expandable list.

--- chmvh_website/resources/static/resources/js/resource_list.js
+++ chmvh_website/resources/static/resources/js/resource_list.js
@@ -1,63 +1,63 @@
 function collapseAll() {
-    $('.resource-category').each(function() {
-        var title = $(this).find('.resource-category__title-bar');
+    $('.expanding-list').each(function() {
+        var title = $(this).find('.expanding-list__title-bar');
         var icon = title.find('i');
-        var resources = $(this).find('.resources');
+        var items = $(this).find('.elist-items');
 
         title.css({
             'border-color': 'transparent',
             'padding-bottom': '0'
         });
         icon.removeClass('fa-minus').addClass('fa-plus');
-        resources.css('display', 'none');
+        items.css('display', 'none');
     });
 }
 
 function expandAll() {
-    $('.resource-category').each(function() {
-        var title = $(this).find('.resource-category__title-bar');
+    $('.expanding-list').each(function() {
+        var title = $(this).find('.expanding-list__title-bar');
         var icon = title.find('i');
-        var resources = $(this).find('.resources');
+        var items = $(this).find('.elist-items');
 
         title.css({
             'border-bottom': '',
             'padding-bottom': ''
         });
         icon.removeClass('fa-plus').addClass('fa-minus');
-        resources.css('display', 'block');
+        items.css('display', 'block');
     });
 }
 
 $(document).ready(function() {
-    $('.resource-category__title-bar').css('cursor', 'pointer');
-    $('.resource-category__toggle').css('display', 'inline-block');
+    $('.expanding-list__title-bar').css('cursor', 'pointer');
+    $('.expanding-list__toggle').css('display', 'inline-block');
 
     collapseAll();
 });
 
-$('.resource-category__title-bar').click(function(e) {
+$('.expanding-list__title-bar').click(function(e) {
     e.preventDefault();
 
     var category = $(this).parent();
-    var categoryTitle = category.find('> .resource-category__title-bar');
+    var categoryTitle = category.find('> .expanding-list__title-bar');
     var icon = $(this).find('i');
-    var resources = category.find('> .resources');
+    var items = category.find('> .elist-items');
 
-    var collapsed = resources.is(':hidden');
+    var collapsed = items.is(':hidden');
 
     if (collapsed) {
         categoryTitle.css({
             'border-bottom': '',
             'padding-bottom': ''
         });
-        resources.slideToggle();
+        items.slideToggle();
         icon.removeClass('fa-plus').addClass('fa-minus');
     } else {
         categoryTitle.css({
             'border-color': 'transparent',
             'padding-bottom': '0'
         });
-        resources.slideToggle();
+        items.slideToggle();
         icon.removeClass('fa-minus').addClass('fa-plus');
     }
 });
