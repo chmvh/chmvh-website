ba64c91e1b6fc2cfd6436c823b0042462dd97ad4
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-18 21:32:58 -0400

    Add admin actions for modifying featured pets.

--- chmvh_website/gallery/admin.py
+++ chmvh_website/gallery/admin.py
@@ -4,6 +4,7 @@ from gallery import models
 
 
 class PatientAdmin(admin.ModelAdmin):
+    actions = ('make_featured', 'remove_featured')
     fieldsets = (
         (None, {
             'fields': (
@@ -14,6 +15,30 @@ class PatientAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'featured', 'deceased')
     list_filter = ('deceased', 'featured')
     search_fields = ('first_name', 'last_name')
+
+    def make_featured(self, request, queryset):
+        rows_updated = queryset.update(featured=True)
+
+        if rows_updated == 1:
+            message_bit = '1 pet'
+        else:
+            message_bit = '{} pets'.format(rows_updated)
+
+        self.message_user(
+            request, '{} marked as featured.'.format(message_bit))
+    make_featured.short_description = 'Mark selected pets as featured'
+
+    def remove_featured(self, request, queryset):
+        rows_updated = queryset.update(featured=False)
+
+        if rows_updated == 1:
+            message_bit = '1 pet'
+        else:
+            message_bit = '{} pets'.format(rows_updated)
+
+        self.message_user(
+            request, '{} removed from featured list.'.format(message_bit))
+    remove_featured.short_description = 'Remove selected from featured pets'
 
 
 admin.site.register(models.Patient, PatientAdmin)
