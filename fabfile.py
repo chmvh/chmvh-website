f830913373a6fdf1bef824f1a82d8629a5801ae9
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-21 01:24:40 -0400

    Automate web server setup.

--- fabfile.py
+++ fabfile.py
@@ -4,8 +4,50 @@ ACTIVATE_ENV = '. env/bin/activate'
 REMOTE_PROJECT_DIR = '/home/chathan/chmvh-website'
 
 
+required_packages = (
+    'git',
+    'libjpeg-dev',
+    'libpq-dev',
+    'postgresql', 'postgresql-contrib',
+    'nginx',
+    'python3-dev', 'python3-pip',
+    'zlib1g-dev',
+)
+
+
+def configure_gunicorn():
+    """Set up gunicorn service."""
+    with cd(REMOTE_PROJECT_DIR):
+        run('sudo mv gunicorn.service /etc/systemd/system')
+
+    run('sudo systemctl start gunicorn')
+    run('sudo systemctl enable gunicorn')
+
+
+def configure_nginx():
+    """Set up nginx."""
+    with cd(REMOTE_PROJECT_DIR):
+        run('sudo mv nginx-config /etc/nginx/sites-available/chmvh-website')
+
+    run('sudo ln -s /etc/nginx/sites-available/chmvh-website '
+        '/etc/nginx/sites-enabled')
+    run('sudo nginx -t')
+    run('sudo systemctl restart nginx')
+
+
+def create_env():
+    """Create a virtualenv if it doesn't exist"""
+    with cd(REMOTE_PROJECT_DIR):
+        run('test -d env || virtualenv --python=python3 env')
+        _in_env('pip install -r requirements.txt')
+
+
 def deploy():
+    prepare_remote()
     update_remote()
+    create_env()
+    configure_gunicorn()
+    configure_nginx()
     generate_static()
     restart_services()
 
@@ -13,8 +55,17 @@ def deploy():
 def generate_static():
     """Generate static files on the remote server."""
     with cd(REMOTE_PROJECT_DIR):
+        _in_env('chmvh_website/manage.py migrate')
         _in_env('chmvh_website/manage.py compilescss')
         _in_env('chmvh_website/manage.py collectstatic -i *.scss --noinput')
+
+
+def prepare_remote():
+    """Install required packages."""
+    run('sudo apt-get update -y && sudo apt-get install -y {}'.format(
+        ' '.join(required_packages)))
+
+    run('pip3 install virtualenv')
 
 
 def restart_services():
@@ -24,8 +75,9 @@ def restart_services():
 
 def update_remote():
     """Pull code onto remote machine."""
-    with cd(REMOTE_PROJECT_DIR):
-        run('git pull')
+    with cd('/home/chathan'):
+        run('if test -d chmvh-website; then cd chmvh-website && git pull; '
+            'else git clone https://github.com/cdriehuys/chmvh-website; fi')
 
 
 def _in_env(command):
