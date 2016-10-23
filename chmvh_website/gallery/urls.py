ba452aea7d3fb30343ff484088a31500d0d6b842
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-23 13:51:22 -0400

    Add api view for listing and creating patients.

--- chmvh_website/gallery/urls.py
+++ chmvh_website/gallery/urls.py
@@ -1,10 +1,17 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
-from gallery import views
+from gallery import api_views, views
+
+
+apiurls = [
+    url(r'^patients/$', api_views.PetListCreateView.as_view(),
+        name='patient-list'),
+]
 
 
 urlpatterns = [
     url(r'^$', views.GalleryIndexView.as_view(), name='index'),
+    url(r'^api/', include(apiurls, namespace='api')),
     url(r'^memoriam/$', views.PetMemoriamView.as_view(), name='pet-memoriam'),
     url(r'^search/$', views.PetSearchView.as_view(), name='search'),
     url(r'^(?P<first_letter>[a-zA-Z])/$', views.PetListView.as_view(),
