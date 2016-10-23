c258b1995bdb870b3818a3dca402b86f2bb85fe9
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-22 23:40:13 -0400

    Add option to overwrite existing thumbnails.

--- chmvh_website/gallery/management/commands/generatethumbnails.py
+++ chmvh_website/gallery/management/commands/generatethumbnails.py
@@ -5,10 +5,24 @@ from gallery.tasks import create_thumbnail
 
 
 class Command(BaseCommand):
-    help = 'Generates thumbnails for the gallery images'
+    help = 'Generates thumbnails for gallery images without thumbnails'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--overwrite',
+            action='store_true',
+            default=False,
+            dest='overwrite',
+            help='Generate thumbnails for all pictures.')
 
     def handle(self, *args, **kwargs):
-        patients = models.Patient.objects.filter(thumbnail=None)
+        if kwargs['overwrite']:
+            self.stdout.write(self.style.WARNING(
+                'Overwriting previously generated thumbnails.'))
+
+            patients = models.Patient.objects.all()
+        else:
+            patients = models.Patient.objects.filter(thumbnail=None)
 
         count = patients.count()
         if count == 0:
@@ -23,6 +37,9 @@ class Command(BaseCommand):
         self.stdout.write('Generating {}...'.format(count_bit))
 
         for patient in patients:
+            if kwargs['overwrite'] and patient.thumbnail:
+                patient.thumbnail.delete()
+
             create_thumbnail(patient)
 
         self.stdout.write(self.style.SUCCESS(
