dfb588f93f22ff24c2347c1993ce95e9aec0054c
Chathan Driehuys <cdriehuys@gmail.com>
2016-10-30 18:42:57 -0400

    Process images when they are saved.
    Closes #38
    Closes #41

--- chmvh_website/gallery/signals.py
+++ chmvh_website/gallery/signals.py
@@ -1,12 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from gallery.tasks import create_thumbnail
+from gallery.tasks import create_thumbnail, process_patient_picture
 
 
 @receiver(post_save, sender='gallery.Patient')
-def send_notifications(sender, instance, update_fields, *args, **kwargs):
+def send_notifications(sender, instance, *args, **kwargs):
     """ Notify users that a reply has been posted """
-    if not instance.thumbnail:
-        if not update_fields or 'thumbnail' not in update_fields:
-            create_thumbnail.delay(instance)
+    process_patient_picture.delay(instance)
--- chmvh_website/gallery/tasks.py
+++ chmvh_website/gallery/tasks.py
@@ -5,10 +5,9 @@ from celery.utils.log import get_task_logger
 from django.conf import settings
 from django.core.files.base import ContentFile
 
-from PIL import Image
+from PIL import ExifTags, Image
 
 from chmvh_website import celery_app
-
 
 _INVALID_FORMAT_ERROR = ("Can't generate thumbnail for {type} filetype. "
                          "(Path: {path})")
@@ -51,5 +50,59 @@ def create_thumbnail(patient, logger=default_logger):
     patient.save(update_fields=['thumbnail'])
 
     logger.info("Generated thumbnail {0}".format(thumb_path))
+
+    return True
+
+
+@celery_app.task
+def process_patient_picture(patient, logger=default_logger):
+    """
+    Process
+    Args:
+        patient:
+            The patient whose image to process.
+        logger:
+            The logger to use for the function.
+    """
+    logger.debug("Processing {file_path}".format(
+        file_path=patient.picture.path))
+
+    image = Image.open(patient.picture.path)
+
+    pil_type = image.format
+    if pil_type in ('JPEG', 'MPO'):
+        pil_type = 'JPEG'   # Saving it as a jpeg makes it easier to process
+    else:
+        # No need to process any other files
+        return
+
+    orientation_key = None
+
+    for key in ExifTags.TAGS.keys():
+        if ExifTags.TAGS[key] == 'Orientation':
+            orientation_key = key
+            break
+
+    if orientation_key is None:
+        logger.warning("No orientation key was found, not rotating.")
+    else:
+        if hasattr(image, '_getexif') and image._getexif() is not None:
+            exif = dict(image._getexif().items())
+            orientation = exif.get(orientation_key)
+
+            logger.debug(
+                "Current picture has orientation {0}".format(orientation))
+
+            if orientation == 3:
+                image = image.rotate(180, expand=True)
+            elif orientation == 6:
+                image = image.rotate(270, expand=True)
+            elif orientation == 8:
+                image = image.rotate(90, expand=True)
+
+    image.save(patient.picture.path, pil_type)
+
+    if not patient.thumbnail:
+        create_thumbnail.delay(patient)
 
     return True
