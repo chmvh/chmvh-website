24895e1527c5b349922a7125145cb3acadd89d46
Chathan Driehuys <cdriehuys@gmail.com>
2017-03-18 15:33:20 -0400

    Add address to email.
    If a user provides their address in the contact form, it is included in
    the email that gets sent.

--- chmvh_website/contact/forms.py
+++ chmvh_website/contact/forms.py
@@ -22,16 +22,37 @@ class ContactForm(forms.Form):
 
     template = loader.get_template('contact/email/message.txt')
 
+    def clean_city(self):
+        """
+        If no city was provided, use a default string.
+        """
+        if not self.cleaned_data['city']:
+            return '<No City Given>'
+
+        return self.cleaned_data['city']
+
     def send_email(self):
         assert self.is_valid()
 
         subject = '[CHMVH Website] Message from {}'.format(
             self.cleaned_data['name'])
 
+        address_line_2_parts = [self.cleaned_data['city'], 'North Carolina']
+        if self.cleaned_data['zipcode']:
+            address_line_2_parts.append(self.cleaned_data['zipcode'])
+
+        address_line_1 = self.cleaned_data['street_address']
+        address_line_2 = ', '.join(address_line_2_parts)
+
+        address = ''
+        if address_line_1:
+            address = '\n'.join([address_line_1, address_line_2])
+
         context = {
             'name': self.cleaned_data['name'],
             'email': self.cleaned_data['email'],
             'message': self.cleaned_data['message'],
+            'address': address,
         }
 
         logger.debug("Preparing to send email")
