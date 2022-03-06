6386cd0a843ed388dbf03aacf82887b3d5afc236
Chathan Driehuys <chathan@driehuys.com>
2022-03-06 15:13:56 -0500

    fix: Settings reference.

--- chmvh_website/custom_storage/s3.py
+++ chmvh_website/custom_storage/s3.py
@@ -1,14 +1,14 @@
 from storages.backends.s3boto3 import S3Boto3Storage, S3ManifestStaticStorage
 
-from django.conf.settings import S3_MEDIA_BUCKET, S3_STATIC_BUCKET
+from django.conf import settings
 
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = S3_MEDIA_BUCKET
+    bucket_name = settings.S3_MEDIA_BUCKET
 
 
 class StaticStorage(S3ManifestStaticStorage):
-    bucket_name = S3_STATIC_BUCKET
+    bucket_name = settings.S3_STATIC_BUCKET
     default_acl = 'public-read'
 
 
